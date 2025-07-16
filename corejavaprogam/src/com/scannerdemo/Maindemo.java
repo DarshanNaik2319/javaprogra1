12ec54d16850ea7e9e947b10f5752f2114ef3d7f
darsh <darsh@DARSHAN>
2025-07-16 11:54:53 +0530

    commit

---
+++ corejavaprogam/src/com/scannerdemo/Maindemo.java
@@ -0,0 +1,26 @@
+package com.scannerdemo;
+import java.util.Scanner;
+public class Maindemo {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the name");
+		String name=sc.nextLine();
+		System.out.println("enter the income");
+		int income=sc.nextInt();
+		
+		Person pp=new Person();
+		pp.setName(name);
+		pp.setIncome(income);
+		
+		Taxcalculation t=new Taxcalculation ();
+		t.calucatetax(pp);
+		
+		System.out.println("after tax calucation");
+		System.out.println(pp);
+		
+	}
+
+}
+
+
